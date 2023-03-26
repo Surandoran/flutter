85ba9086bd6b5c30043d358b53ded2810821a705
Son <70love07@naver.com>
2023-03-26 23:10:12 +0900

    #2 Widget 분리
    Butto nWdiget 분리 및 생성

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,34 +1,95 @@
 import 'package:flutter/material.dart';
-
-class Player {
-  String name;
-
-  Player({required this.name});
-}
+import 'package:toonflix/widgets/Button.dart';
 
 void main() {
-  var son = Player(
-    name: 'son',
-  );
-
-  runApp(App());
+  runApp(const App());
 }
 
 // runApp(Widget app) 여기서 Widget이란 레고 블럭
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // Scaffold는 화면의 구성 및 구조에 관한 것들을 가지고 있는 Widget
       home: Scaffold(
-        // appbar는 화면의 상단을 구성
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Hello flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: const [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 44,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Button(
+                    text: 'Transfer',
+                    bgColor: Color(0xFFF1B33B),
+                    textColor: Colors.black,
+                  ),
+                  Button(
+                    text: 'Request',
+                    bgColor: Color(0xFF1F2123),
+                    textColor: Colors.white,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
